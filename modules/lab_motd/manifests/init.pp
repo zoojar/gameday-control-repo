079018d35afe2989371c17e1fe5330f3e60ce5c1
root <root>
2021-01-27 13:46:52 +0000

    motd

---
+++ modules/lab_motd/manifests/init.pp
@@ -0,0 +1,16 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include lab_motd
+class lab_motd {
+  file { 'ect/motd':
+    ensure  => file,
+    path    => '/etc/motd/',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'This system is managed by Puppet using the dev branch',
+    }
+}
