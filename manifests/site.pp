fc8fdb342947ae846da181ab433ec29e5afb4002
root <root>
2021-01-28 09:26:41 +0000

    updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,17 +5,10 @@ File { backup => false }
 ## Node Definitions ##
 
 node default {
-  # Find the first instance of `classes` in hiera data and includes unique values. Does not merge results.
-  $classes = lookup('classes', Variant[String,Array[String]])
-  case $classes {
-    String[1]: {
-      include $classes
-    }
-    Array[String[1],1]: {
-      $classes.unique.include
-    }
-    default: {
-      fail('This node did not receive any classification')
-    }
+  $role = lookup('role', false)
+  if $role != false {
+    hiera_include('role')
+  } else {
+    include 'role::undefined'
   }
 }
