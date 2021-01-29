b287f261a5a43c1117220f539d1d24ffcde8df1d
root <root>
2021-01-29 21:07:28 +0000

    clientcert updates

--- site-modules/profile/manifests/motd.pp
+++ site-modules/profile/manifests/motd.pp
@@ -1,10 +1,10 @@
 # @summary A short summary of the purpose of this class
 #
 # A description of what this class does
-#
+# Used %{trusted.certname} from the trusted facts.
 # @example
 class profile::motd {
   class { 'motd':
-    content => "This instance: %{trusted.hostname} is managed by Puppet.\n",
+    content => "This instance: %{trusted.certname} is managed by Puppet.\n",
   }
 }
