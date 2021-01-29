15cd0fefb69444499a5237ffaf76004daa2355d6
root <root>
2021-01-29 16:37:41 +0000

    added users and groups

--- site-modules/profile/spec/classes/motd_spec.rb
+++ site-modules/profile/spec/classes/motd_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe 'profile::nginx' do
+describe 'profile::motd' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
