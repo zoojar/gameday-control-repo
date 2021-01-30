7d9dcb81832f6b896fc5066512024422a624068c
root <root>
2021-01-30 13:38:36 +0000

    ntp updates

--- site-modules/profile/manifests/ntp.pp
+++ site-modules/profile/manifests/ntp.pp
@@ -5,7 +5,7 @@
 # @example
 #   include profile::ntp
 class profile::ntp {
-  #class { 'ntp':
+  class { 'ntp':
   #  servers => [ 'server 0.uk.pool.ntp.org', 'server 1.uk.pool.ntp.org', 'server 2.uk.pool.ntp.org' , 'server 3.uk.pool.ntp.org' ]
-  #}
+  }
 }
