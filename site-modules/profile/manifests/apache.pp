47e38f91f33e47e9208bc986073de57a94d4e682
root <root>
2021-02-01 16:30:36 +0000

    updates

--- site-modules/profile/manifests/apache.pp
+++ site-modules/profile/manifests/apache.pp
@@ -1,10 +1,12 @@
 #   include profile::apache
 class profile::apache {
   include apache
-  apache::vhost { 'localhost':
-    ensure => present,
+  #apache::vhost { 'localhost':
+  #  ensure => present,
   }
   file { 'index.html':
+    ensure  => 'present',
+    mode    => '0644',
     path    => '/var/www/html',
     content => '<h1>Hello World! </h1> <br/> <h2> Test page </h2>',
   }
