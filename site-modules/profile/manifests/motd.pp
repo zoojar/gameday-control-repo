86572b1c36ec0fd63cfccc33ae34d7cdd5518538
root <root>
2021-02-01 10:08:15 +0000

    updates

--- site-modules/profile/manifests/motd.pp
+++ site-modules/profile/manifests/motd.pp
@@ -1,8 +1,10 @@
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
-# Used %{trusted.certname} from the trusted facts.
-# @example
+# This motd declares a string as assigns the var $content to it, then a new class declared 'motd'
+# and content => $content, this is a lookup for hiera to search for the var $content
+# and return it's value, in this case the value is in host1.mllab.yaml - profile::motd::content: ""
+# This works due to hiera.yaml having the following:
+# paths:
+# - "nodes/%{trusted.certname}.yaml" this is a trusted fact "certname" : "host1.mllab", and can be found under
+# Trusted in the PE console Nodes - Facts.
 class profile::motd (
   String $content
 ){
