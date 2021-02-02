fed9b2cc651913ecd92c9fe7a80fc6f7ab957241
root <root>
2021-02-02 13:22:06 +0000

    updates

--- site-modules/profile/manifests/webserver.pp
+++ site-modules/profile/manifests/webserver.pp
@@ -8,15 +8,11 @@ class profile::webserver {
   class { 'apache': }
 
 apache::vhost { 'mllab.com':
-  server_name   => 'mllab.com',
   serveraliases => [],
   docroot       => '/var/www/mllab',
   docroot_owner => 'www-data',
   docroot_group => 'www-data',
   port          => '80',
-  directories   => [ { path => '/var/www/', allow => 'from all', allow_override => ['All'] }   ],
-  env_variables => [],
-  priority      => '1',
 }
 
 file { 'site-content':
