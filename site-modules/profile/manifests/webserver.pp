010f1b2729c4ee6b43bde1d5341934b63230a7dc
root <root>
2021-02-02 17:13:46 +0000

    updates

--- site-modules/profile/manifests/webserver.pp
+++ site-modules/profile/manifests/webserver.pp
@@ -7,12 +7,21 @@
 class profile::webserver {
   class { 'apache': }
 
-apache::vhost { 'mllab':
+$apache_vhost = lookup('apache_vhost')
+$docroot = lookup('docroot')
+$docroot_path = lookup('docroot_path')
+$docroot_group = lookup('docroot_group')
+$docroot_owner = lookup('docroot_owner')
+$web_content = lookup('web_content')
+$web_port = lookup('web_port')
+$web_perm = lookup('web_perm')
+
+apache::vhost { $apache_vhost:
   serveraliases => [],
-  docroot       => '/var/www/html',
-  docroot_owner => 'apache',
-  docroot_group => 'apache',
-  port          => '80',
+  docroot       => $docroot,
+  docroot_owner => $docroot_owner,
+  docroot_group => $docroot_group,
+  port          => $web_port,
 }
 
 file { '/var/www/html/':
@@ -21,8 +30,8 @@ file { '/var/www/html/':
 
 file { 'site-content':
     ensure  => present,
-    mode    => '0644',
-    path    => '/var/www/html/index.html/',
-    content => '<h1>Welcome to the VMER Game day !!</h1> <br/> <h2> This is a test page </h2>',
+    mode    => $web_perm,
+    path    => $docroot_path,
+    content => $web_content,
   }
 }
